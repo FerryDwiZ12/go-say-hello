2eca696d851fa7f501754f890ec9a5e8a794b462
Ferry Dwi Zulkhifli <ferrydwizulkhifli@gmail.com>
2023-09-24 19:46:22 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/FerryDwiZ12/go-say-hello
+module github.com/FerryDwiZ12/go-say-hello/v2
 
 go 1.18
--- say_hello.go
+++ say_hello.go
@@ -1,7 +1,7 @@
 package go_say_hello
 
-func SayHello() string{
-	return "Hello World"
+func SayHello(name string) string{
+	return "Hello World" + name
 }
 
 // menbuat tag version di github git tag v1.0.0 lalu git push origin v1.0.0
